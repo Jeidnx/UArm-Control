ee41694ca7c4e524a3db962eaa9ca83d3c428033
Jeidnx <jeidnx@domainhier.de>
2022-11-10 12:50:46 +0100

    Better joystick handling; Remove out dir from repo

--- src/Joystick.java
+++ src/Joystick.java
@@ -8,52 +8,22 @@ public class Joystick extends Thread{
         this.serial.open();
         this.start();
     }
-    private void sleep(){
-        return;
-        /*try{
-            Thread.sleep(2000);
-        } catch (Exception ignored) {}*/
+    private void flush(){
+        this.serial.readBytes();
     }
-
     public void run() {
         System.out.println("Joystick thread started");
         while (true){
             if(this.serial.dataAvailable() > 0){
-                String args = this.serial.readLine();
-                if(args == null){
-                    continue;
+                String arg = this.serial.readLine();
+                switch (arg) {
+                    case "b" -> board.dropOrPickup();
+                    case "u" -> board.moveRelative(-1, 0);
+                    case "d" -> board.moveRelative(1, 0);
+                    case "r" -> board.moveRelative(0, 1);
+                    case "l" -> board.moveRelative(0, -1);
                 }
-                String[] args2 = args.split(" ");
-                if(args2.length != 3){
-                    continue;
-                }
-                if(args2[0].equals("1")){
-                    board.dropOrPickup();
-                }
-
-                    int xpos = Integer.parseInt(args2[1]);
-                    if(xpos > 700){
-                        System.out.println("Right");
-                        board.moveRelative(0, 1);
-                        sleep();
-                    }
-                    if(xpos < 300){
-                        System.out.println("Left");
-                        board.moveRelative(0, -1);
-                        sleep();
-                    }
-
-                    int ypos = Integer.parseInt(args2[2]);
-                    if(ypos > 700){
-                        System.out.println("Up");
-                        board.moveRelative(-1, 0);
-                        sleep();
-                    }
-                    if(ypos < 300){
-                        System.out.println("Down");
-                        board.moveRelative(1, 0);
-                        sleep();
-                    }
+                this.flush();
             }
         }
 
